4708090d125cd311d18bc84f48b389c2ba0e41ce
Dzulfikar <dzullocal@gmail.com>
2022-02-17 21:33:06 +0700

    first

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('backend/template/header');
+        $this->load->view('backend/template/sidebar');
+        $this->load->view('backend/dashboard/view');
+        $this->load->view('backend/template/footer');
+    }
+}
